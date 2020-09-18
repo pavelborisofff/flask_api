ea16c40b677800030ef1dc07e45fa41addc669da
pavelborisov <pavel.borisofff@gmail.com>
2020-09-19 01:49:30 +0300

    fixed a little

--- flask_catch_all.py
+++ flask_catch_all.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import json
 
 app = Flask(__name__)
 
@@ -6,12 +7,19 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''}, methods=['GET', 'POST'])
 @app.route('/<path:path>', methods=['GET', 'POST'])
 def catch_all(path):
-    print(request.headers)
-
+    print('\n\n==== NEW CONNECTION ====\n', request.headers)
     if request.method == 'POST':
-        return {'ip': str(request.remote_addr), 'path': path, 'json': request.get_json(), 'form': request.form.to_dict()}, 201
+        return {'ip': request.headers.get('X-Real-IP', '127.0.0.1'),
+		'path': path,
+		'json': request.get_json(),
+		'form': request.form.to_dict(),
+		'data': json.loads(request.data, encoding='utf8')
+		}, 201
     else:
-        return {'ip': str(request.remote_addr), 'path': path, 'args': request.args.to_dict()}, 200
+        return {'ip': request.headers.get('X-Real-IP', '127.0.0.1'),
+		'path': path,
+		'args': request.args.to_dict()
+		}, 200
 
 
 if __name__ == '__main__':
